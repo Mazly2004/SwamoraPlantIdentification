241e6688703785ef0f57feeb613ba3171c98083c
Oliver Mazorodze <olivermazorodze1@gmail.com>
2026-05-24 13:43:17 +0200

    Fixed

--- SwamoraPlant.ui/src/routes/__root.tsx
+++ SwamoraPlant.ui/src/routes/__root.tsx
@@ -1,11 +1,13 @@
 import { Outlet, createRootRoute } from '@tanstack/react-router'
 import { ThemeProvider } from '@/components/theme-provider'
+import { PlantLoader } from '@/components/PlantLoader'
 
 function RootComponent() {
   return (
     <ThemeProvider defaultTheme="light" storageKey="farmsight-theme">
       <div className="min-h-screen bg-background text-foreground flex flex-col">
         <Outlet />
+        <PlantLoader />
       </div>
     </ThemeProvider>
   )
